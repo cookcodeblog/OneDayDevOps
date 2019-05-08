71c685e98f3d629f7698f1fc425be4dae9fbb41f
cookcodeblog <cookcode_me@sina.com>
2019-05-08 23:18:05 +0800

    Filter out docker0 ip

--- components/utils/get_ip.sh
+++ components/utils/get_ip.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-ip addr show | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
+ip addr show | grep -v 'docker0' | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
