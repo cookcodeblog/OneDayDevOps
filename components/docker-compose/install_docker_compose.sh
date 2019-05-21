96f505476af670b523880be4ee846158ab03e363
cookcodeblog <cookcode_me@sina.com>
2019-05-21 17:03:25 +0800

    Support given docker-compose version

--- components/docker-compose/install_docker_compose.sh
+++ components/docker-compose/install_docker_compose.sh
@@ -9,7 +9,13 @@ then
   cd ${SCRIPT_DIR}
 fi
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+VERSION="$1"
+if [ ! -n "${VERSION}" ]; then
+    VERSION="1.24.0"
+fi
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
