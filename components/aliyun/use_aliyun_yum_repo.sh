dd25d2fba396027ae53f6a9a345b725fa5ef6ad9
cookcodeblog <williamsrlin@outlook.com>
2019-06-30 22:03:19 +0800

    Use Aliyun epel-7 repo

--- components/aliyun/use_aliyun_yum_repo.sh
+++ components/aliyun/use_aliyun_yum_repo.sh
@@ -3,6 +3,7 @@
 set -e
 
 curl -o /etc/yum.repos.d/CentOS7-Aliyun.repo https://raw.githubusercontent.com/cookcodeblog/OneDayDevOps/master/components/aliyun/CentOS7-Aliyun.repo
+curl -o /etc/yum.repos.d/epel-7-Aliyun.repo https://raw.githubusercontent.com/cookcodeblog/OneDayDevOps/master/components/aliyun/epel-7-Aliyun.repo
 
 yum clean all
 yum makecache
