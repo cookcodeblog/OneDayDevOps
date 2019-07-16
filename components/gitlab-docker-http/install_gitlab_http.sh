cb6f8004a6a4535b7ba3eb8f3b317e84146921ab
cookcodeblog <williamsrlin@outlook.com>
2019-07-16 17:06:26 +0800

    Update gitlab docker install script

---
+++ components/gitlab-docker-http/install_gitlab_http.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Locate shell script path
+SCRIPT_DIR=$(dirname $0)
+if [ ${SCRIPT_DIR} != '.' ]
+then
+  cd ${SCRIPT_DIR}
+fi
+
+
+# Open ports on Firewall
+../../components/utils/open_firewall_port.sh 30080
+../../components/utils/open_firewall_port.sh 30022
+
+# Run GitLab with Docker Compose
+mkdir -p /opt/gitlab
+
+cp docker-compose.yml /opt/gitlab
+
+cd /opt/gitlab
+docker-compose up -d
+
