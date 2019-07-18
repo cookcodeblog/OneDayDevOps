cba4f9c37737dcf8cde9196e0e0fd3d6ea0b92a9
cookcodeblog <williamsrlin@outlook.com>
2019-07-18 10:53:00 +0800

    Update use aliyun yum script

--- components/aliyun/use_aliyun_yum_repo.sh
+++ components/aliyun/use_aliyun_yum_repo.sh
@@ -2,8 +2,18 @@
 
 set -e
 
-curl -o /etc/yum.repos.d/CentOS7-Aliyun.repo https://raw.githubusercontent.com/cookcodeblog/OneDayDevOps/master/components/aliyun/CentOS7-Aliyun.repo
-curl -o /etc/yum.repos.d/epel-7-Aliyun.repo https://raw.githubusercontent.com/cookcodeblog/OneDayDevOps/master/components/aliyun/epel-7-Aliyun.repo
+# Locate shell script path
+SCRIPT_DIR=$(dirname $0)
+if [ ${SCRIPT_DIR} != '.' ]
+then
+  cd ${SCRIPT_DIR}
+fi
+
+#curl -o /etc/yum.repos.d/CentOS7-Aliyun.repo https://raw.githubusercontent.com/cookcodeblog/OneDayDevOps/master/components/aliyun/CentOS7-Aliyun.repo
+#curl -o /etc/yum.repos.d/epel-7-Aliyun.repo https://raw.githubusercontent.com/cookcodeblog/OneDayDevOps/master/components/aliyun/epel-7-Aliyun.repo
+
+cp CentOS7-Aliyun.repo /etc/yum.repos.d/
+cp epel-7-Aliyun.repo /etc/yum.repos.d/
 
 yum clean all
 yum makecache
